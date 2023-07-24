9996865a11026afb2415dcbd1e4ce9032986d202
Taha Ansari <taha.m.ansari@gmail.com>
2023-07-24 18:11:30 +0530

    wip

---
+++ Components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../styles/Layout.module.css";
+
+const Layout = () => {
+  return (
+    <div className={Layout.navigation}>
+      <h1>Layout</h1>
+    </div>
+  );
+};
+
+export default Layout;
